acd0c61836684be538903277c56ba2e00a490a77
Zioun <jionkhan0@gmail.com>
2024-03-04 00:07:17 +0600

    successfully done :)

--- js/main.js
+++ js/main.js
@@ -20,42 +20,60 @@ const fecthPost = async (search) =>{
         }
         const div = document.createElement("div");
         div.innerHTML = `
-    <div class="bg-[#F3F3F5] p-[30px] md:p-[40px] rounded-[24px] flex flex-col md:flex-row gap-[24px] justify-center hover:bg-[#F2F2FF]">
-        <div>
-            <div id="badge-area" class="indicator">
-                ${badgeArea.innerHTML}
-                <div class="grid h-[72px] max-w-[72px] bg-base-300 place-items-center rounded-2xl"><img class="rounded-2xl" src="${postArray.image}" /></div>
+        <div class="bg-[#F3F3F5] p-[30px] md:p-[40px] rounded-[24px] flex flex-col md:flex-row gap-[24px] justify-center hover:bg-[#F2F2FF]">
+            <div>
+                <div id="badge-area" class="indicator">
+                    ${badgeArea.innerHTML}
+                    <div class="grid h-[72px] max-w-[72px] bg-base-300 place-items-center rounded-2xl"><img class="rounded-2xl" src="${postArray.image}" /></div>
+                </div>
             </div>
-        </div>
-        <div>
-            <div class="flex gap-[20px]">
-                <h1 class="text-[14px] font-medium inter"># <span>${postArray.category}</span></h1>
-                <h2 class="text-[14px] font-medium inter">Author : <span>${postArray.author.name}</span></h2>
-            </div>
-            <div class="border-dashed border-b-[2px] border-[#BABACA]">
-                <h1 class="mulish font-bold text-[20px] mt-[12px] mb-[16px]">${postArray.title}</h1>
-                <p class="max-w-[569px] md:w-[569px] mb-[20px] inter text-[16px] font-normal text-[#6C6D7D]">${postArray.description}</p>
-            </div>
-            <div class="flex flex-col md:flex-row  justify-between mt-[20px]">
-                <div class="flex max-w-[283px] gap-[20px] md:gap-[25px]">
-                    <div class="flex gap-[12px]">
-                        <span class="inter text-[16px] font-normal text-[#6C6D7D]">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-                            </svg>
-                        </span>
-                        <span class="inter text-[16px] font-normal text-[#6C6D7D]">${postArray.comment_count}</span>
+            <div>
+                <div class="flex gap-[20px]">
+                    <h1 class="text-[14px] font-medium inter"># <span>${postArray.category}</span></h1>
+                    <h2 class="text-[14px] font-medium inter">Author : <span>${postArray.author.name}</span></h2>
+                </div>
+                <div class="border-dashed border-b-[2px] border-[#BABACA]">
+                    <h1 class="mulish font-bold text-[20px] mt-[12px] mb-[16px]">${postArray.title}</h1>
+                    <p class="max-w-[569px] md:w-[569px] mb-[20px] inter text-[16px] font-normal text-[#6C6D7D]">${postArray.description}</p>
+                </div>
+                <div class="flex flex-col md:flex-row  justify-between mt-[20px]">
+                    <div class="flex max-w-[283px] gap-[20px] md:gap-[25px]">
+                        <div class="flex gap-[12px]">
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                                </svg>
+                            </span>
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">${postArray.comment_count}</span>
+                        </div>
+                        <div class="flex gap-[12px]">
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg> 
+                            </span>
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">${postArray.view_count}</span>
+                        </div>
+                        <div class="flex gap-[12px]">
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                            </span>
+                            <span class="inter text-[16px] font-normal text-[#6C6D7D]">${postArray.posted_time} <span>m</span></span>
+                        </div>
                     </div>
-                    <!-- Similar blocks for view count and posted time -->
-                </div>
-                <div class="mt-5 md:mt-0 flex justify-center">
-                    <button onclick="markItem('${postArray.title}','${postArray.view_count}')"><img src="images/email-1.png" alt="email"></button>
+                    <div class="mt-5 md:mt-0 flex justify-center">
+                    <button onclick="markItem('${postArray.title.replace("'", "")}','${postArray.view_count}')"><img src="images/email-1.png" alt="email"></button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-        postArea.appendChild(div);
+        `;
+        setTimeout(()=>{
+            postArea.appendChild(div);
+        },2000)
     });
     preLoader(false);
     }else{
@@ -126,10 +144,11 @@ const fecthPost = async (search) =>{
                 </div>
             </div>
         </div>
-        `
-        postArea.appendChild(div);
+        `;
+        setTimeout(()=>{
+            postArea.appendChild(div);
+        },2000)
     });
-    console.log(responseData)
     preLoader(false);
     }
     
@@ -150,12 +169,16 @@ const preLoader = (loader) => {
     if (loader) {
         preLoaderIcon.classList.remove("hidden");
     }else{
-        preLoaderIcon.classList.add("hidden");
+        setTimeout(()=>{
+            preLoaderIcon.classList.add("hidden");
+        },2000)
     }
 }
 
 const markItem = (title,view)=>{
-    console.log(title,view)
+    const markCount = document.getElementById("mark-count");
+    let count = parseInt(markCount.innerText);
+    markCount.innerText = ++count;
     const markArea = document.getElementById("mark-area");
     const div = document.createElement("div");
     div.className = "flex items-center justify-between bg-[#FFFFFF] px-[16px] py-[15px] rounded-[16px]";
